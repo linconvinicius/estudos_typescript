70aab3878855aaf9d5f305284a80d8a263780964
Lincon Vinicius <lvini_@live.com>
2025-08-06 18:35:38 -0300

    atualizando

--- src/main.ts
+++ src/main.ts
@@ -5,4 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3333);
 }
-bootstrap();
+void bootstrap().catch(error => {
+  console.error('Failed to bootstrap application:', error);
+  process.exit(1) 
+});
